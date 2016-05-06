327d2d7f62869f792a280c3abb085a3abc7e9ac5
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-06 12:31:49 -0500

    created encrypt and decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,41 @@
+def encrypt(password)
+	index = 0 
+	encrypt_password = password
+	while index < password.length
+		if password[index] == "z"
+			password[index]= "a"
+		elsif password[index] == " "
+			encrypt_password[index] = password[index]
+		else
+			encrypt_password[index] = password[index].next
+			index += 1
+		end
+	end
+	return encrypt_password
+end
+
+
+def decrypt(password)
+	index = 0
+	variable_range = "abcdefghijklmnopqrstuvwxyz"
+	varible_index = 0
+	decrypt_password = password
+	while index < password.length
+		if password[index] == " "
+			decrypt_password[index] = password[index]
+			index +=1
+		else
+			until password[index] ==  variable_range[variable_index]
+			variable_index += 1
+			end
+			decrypt_password[index] = variable_range[variable_index - 1]
+			index += 1
+		end
+		variable_index = 0 
+	end
+	return decrypt_password
+end
+
+
+
+
