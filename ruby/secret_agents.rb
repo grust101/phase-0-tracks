a05343ded21be1cf050632b90ca43897d46b446a
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-08 22:05:51 -0500

    fixed encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -8,18 +8,17 @@ def encrypt(password)
 	index = 0
 	while index < password.length
 		if password[index] == "z"
-			password[index] = "`"
+			password[index] = "a"
 		else
-			print password[index].next
-			index += 1
+			password[index] = password[index].next
 		end
+		index += 1
 	end
+	return password
 end
 
-encrypt("abc")
-puts ""
-encrypt("zed")
-puts ""
+#encrypt("abc")
+#encrypt("zed")
 
 =begin
 	-Ask the user to key in a word
@@ -31,32 +30,39 @@ def decrypt(password)
 	index = 0
 	while index < password.length
 		if password[index] == "a"
-			password[index] = "{"
+			password[index] = "z"
 		else
-			print (password[index].ord-1).chr
-			index += 1
+			password[index] = (password[index].ord-1).chr
 		end
+		index += 1
+	end
+	return password
+end
+
+#decrypt("bcd")
+#decrypt("afe")
+
+#password = "swordfish"
+#puts decrypt(encrypt(password))
+#Method within a method works because it processes whatever is in parenthesis first
+
+#Keep asking until a valid answer: encrypt or decrypt
+valid = false
+until valid
+	puts "Would you like to encrypt(e) or decrypt(d) a password?"
+	choice = gets.chomp
+	if choice == "e" || choice == "d"
+		valid = true
 	end
 end
 
-decrypt("bcd")
-puts ""
-decrypt("afe")
-puts ""
-
-# Ask the for choice of encrypt or decrypt
-puts "Would you like to encrypt(e) or decrypt(d) a password?"
-choice = gets.chomp
-
-# Ask the user to input a password
+#Ask the user to input a password
 puts "Key in your password."
 password = gets.chomp.downcase
 
-# Convert password to the method of choice
+#Convert password to the method of choice
 if choice == "e"
 	puts encrypt(password)
 elsif choice == "d"
 	puts decrypt(password)
 end
-
-puts "Have a nice day!"
