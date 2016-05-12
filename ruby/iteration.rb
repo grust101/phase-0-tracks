45746a17136251b99af718ee0cd3c0f5a2657ffe
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-11 22:09:12 -0500

    completed release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,7 @@
+# 5.4 
+# Will Friebel and Georgia Rust
+
+
 ingredients = [
 	'dough',
 	'tomato sauce',
@@ -13,17 +17,54 @@ animal_colors = {
 	bird: 'blue'
 }
 
-ingredients.each do |ingredient|
-	puts "#{ingredient}!!"
-end
+phone_numbers = {
+	Jan: 6502347892,
+	Bob: 7015672849,
+	Sam: 3034589300,
+	Dan: 2183459802
+}
 
-ingredients.map! do |ingredient|
-	ingredient.reverse
-end
 
-puts ingredients
+# Release 1: 
 
-animal_colors.each do |animal, color|
-	puts color.upcase
-end
+#ingredients.each do |ingredient|
+	#puts "#{ingredient}!!"
+#end
+
+#ingredients.map! do |ingredient|
+	#ingredient.reverse
+#end
+#puts ingredients
+
+#animal_colors.each do |animal, color|
+#	puts color.upcase
+#end
+
+# Release 2: 
+
+# 1. 
+
+#ingredients.delete_if {|ingredient| ingredient.length > 6}
+#p ingredients
+
+#animal_colors.delete_if {|animal, color| color.include? "e"}
+#p animal_colors
+
+# 2. 
+#ingredients.keep_if {|ingredient| ingredient.start_with?("c")}
+#p ingredients
+
+#phone_numbers.keep_if {|name, number| number.even?}
+#p phone_numbers
+
+# 3. 
+#p phone_numbers.select {|name, number| number.even?}
+
+#p ingredients.select {|ingredient| ingredient.include? "p"}
+
+# 4. 
+#p ingredients.drop_while {|ingredient| ingredient.length > 6}
+
+#p phone_numbers.drop_while {|name, number| number.even?}
+ 
 
