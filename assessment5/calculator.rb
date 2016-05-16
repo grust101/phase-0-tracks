54903322307c302202c18564ce747a5c9f76aaf7
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-16 16:14:45 -0500

    worked on UI, need to fix logic

--- assessment5/calculator.rb
+++ assessment5/calculator.rb
@@ -2,27 +2,33 @@
 # takes 3 parameters: integer, operator string, and another integer
 # makes calculations for operators + - / * 
 
+# LOGIC
 
-# def calculate(integer, operator, integer2)
-# 	operator = ['+', '-', '*', '/']
-# 	operator.map { |o| integer.method(o).(integer2) }	
-# end
-
-#  p calculate(4, '+', 5)
-
-
-input = ''
-while input != 'done'
-	p "Please enter the equation you want me to solve! (Enter 'done' when you've had enough)"
-	input = gets.chomp
-	if input != 'done'
-		output = "Answer:" + calculate(input)
-		p output
-	else
-		p "Goodbye!"
-
-	end
+def calculate(integer, operator, integer2)
+	
 end
 
 
-p Readline::HISTORY.to_a
+# TEST CODE
+ p calculate(4, "+", 5)
+
+
+
+# USER INTERFACE
+
+# can_calc = true
+
+# while can_calc
+# 	# get an equation to solve from user
+# 	p "Please enter the equation you want me to solve! (Enter 'done' when you've had enough)"
+# 	entered_equation = gets.chomp
+# 	# if the user is done, stop loop
+# 	break if entered_equation == 'done'
+# 	# otherwise keep calculating
+# 	can_calc = calculate(integer, operator, integer2)
+
+# 	end
+# end
+
+
+# p Readline::HISTORY.to_a
