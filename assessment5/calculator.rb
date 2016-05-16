fdfd4911a5fc571ff63200988a543f4046acb129
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-16 14:14:38 -0500

    figured out method that almost works. Couldnt get UI right

--- assessment5/calculator.rb
+++ assessment5/calculator.rb
@@ -3,27 +3,26 @@
 # makes calculations for operators + - / * 
 
 
-def calculate(operation)
-	new_operation = operation.split(",")
-	
-	
+# def calculate(integer, operator, integer2)
+# 	operator = ['+', '-', '*', '/']
+# 	operator.map { |o| integer.method(o).(integer2) }	
+# end
+
+#  p calculate(4, '+', 5)
+
+
+input = ''
+while input != 'done'
+	p "Please enter the equation you want me to solve! (Enter 'done' when you've had enough)"
+	input = gets.chomp
+	if input != 'done'
+		output = "Answer:" + calculate(input)
+		p output
+	else
+		p "Goodbye!"
+
 	end
-	
 end
 
 
- p calculate("4, +, 5")
-
-
-# input = ''
-# while input != 'done'
-# 	p "Please enter the equation you want me to solve! (Enter 'done' when you've had enough)"
-# 	input = gets.chomp
-# 	if input != 'done'
-# 		output = "Answer:" + adding_int(input)
-# 		p output
-# 	else
-# 		p "Goodbye!"
-
-# 	end
-# end
+p Readline::HISTORY.to_a
