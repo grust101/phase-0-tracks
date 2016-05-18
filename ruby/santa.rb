6d13bf9e9855659b5206a04f0172594e9fcfc056
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-18 13:58:17 -0500

    Created setter and getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -27,19 +27,59 @@ class Santa
 		p "Ethnicity: #{@ethnicity}"
 	end
 
-	def age(years)
-		p "Santa's current age: #{years} years old."
-	end
 
+# Attribute changing methods
+def celebrate_birthday
+	age = @age + 1
+	p "Santa is now #{age} years old!"
+end
 
+def get_mad_at(name)
+	if @reindeer_ranking.include?(name) == true
+	 index_to_rotate = @reindeer_ranking.index(name)
+	 p @reindeer_ranking.insert(8, @reindeer_ranking.delete_at(index_to_rotate))
+	else
+		p "No one's in trouble"
+end	
+
+# Create a setter method that allows @gender to be reassigned outside the class def
+def gender= (new_gender)
+	@gender = new_gender
+end
+
+# Getter methods for age and ethnicity
+
+def age
+	@age
+end
+
+def ethnicity
+	@ethnicity
+end
 
 end
 
-# santa = Santa.new("Cis Female", "Asian American")
-# santa.about
-# santa.speak
-# santa.eat_milk_and_cookies("Snickerdoodle")
+santa = Santa.new("Cis Female", "Asian American")
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("Snickerdoodle")
+santa.celebrate_birthday
+santa.get_mad_at("Vixen")
 
+santa.gender= "African American"
+p "Santa is #{santa.gender}."
+
+p " Santa is #{santa.age} years old!"
+p "Santa's ethnicity is #{santa.ethnicity}."
+
+
+
+# santa = []
+
+# gender_options = ["female", "male", "agender", "bigender", "cis female", "male"]
+# ethnicity_options = ["Hispanic", "Asian American", "African American", "Perfer not to answer", "White"]
+# gender_options.length.times do |i|
+# 	santa << Santa.new(gender_options[i], ethnicity_options[i])
 
 
 
@@ -52,11 +92,12 @@ end
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+
 end
-
-
