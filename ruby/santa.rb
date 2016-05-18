7c808227e6dc5e7f13f2276008c0e3945e4b7436
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-18 10:29:20 -0500

    created Santa class and initialized speak and eat milk and cookies instances

---
+++ ruby/santa.rb
@@ -0,0 +1,22 @@
+# 
+
+class Santa 
+	def initialize
+		p "initializing Santa instance...."
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		
+		p "That was a good #{cookie}!"
+	end
+
+end
+
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("Snickerdoodle")
+
