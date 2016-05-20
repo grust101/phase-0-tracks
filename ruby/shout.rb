13cfe83bded72e8449d6f6a686ce42854cbc0ed8
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-19 19:06:19 -0500

    added yell angrily and happily methods and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
 	# Methods will go here 
+	def self.yell_angrily(words)
+		words + "!!!" + ":("
+	end
+
+	def self.yell_happily(words)
+		words.upcase + "!!!!" + ":D"
+	end
+
+
+p self.yell_angrily("You are so mean")
+p self.yell_happily("You are the nicest human on Earth")
+
 end
