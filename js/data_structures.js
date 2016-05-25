8c63948a12adbf67c7ecf5c66fe1760b6987cfa5
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-25 11:39:56 -0500

    add car function and run testCar

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,29 @@ colors.push("pink");
 names.push("Todd");
 
 console.log(colors, names);
+
+var horseColors = {};
+
+for (var i = 0; i < colors.length; i++) {
+	horseColors[names[i]] = colors[i];
+}
+
+console.log(horseColors);
+
+
+function Car(color, year, americanMade) {
+	console.log("This car: ", this);
+
+	this.color = color;
+	this.year = year; 
+	this.americanMade = americanMade;
+
+	this.zoom = function() {console.log("ZOOOOOM!");};
+
+	console.log("CAR DESCRIPTION COMPLETE");
+}
+
+var testCar = new Car("black", 2007, true);
+console.log(testCar);
+
+testCar.zoom();
