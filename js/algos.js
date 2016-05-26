2b3fdd4c3859194a92653e3bd3b91ce749e9bc96
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-26 12:42:17 -0500

    add function to print the longest phrase

--- js/algos.js
+++ js/algos.js
@@ -2,8 +2,26 @@
 // find the length of each word in the array
 // sort through the array for the vairable with the longest length
 
+
+
 var array = ["long phrase", "longer phrase", "longest phrase"];
 
+
+
+var length = 0
+var longest;
+
+
 for (var i = 0; i < array.length; i++) {
-	cosole.log(array[i])
+	if(array[i].length > length){
+		var length = array[i].length;
+		longest = array[i];
+	}
 }
+
+console.log(longest);
+
+// var array.each do |phrase| 
+// 	phraseLengths = phrase.length
+// console.log(phraseLengths)
+// end
