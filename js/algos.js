632d2b74881b5540464e8edfe3a499fcd4be4baa
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-05-26 12:23:33 -0500

    create algos page and array

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+// create array variables 
+// find the length of each word in the array
+// sort through the array for the vairable with the longest length
+
+var array = ["long phrase", "longer phrase", "longest phrase"];
+
+for (var i = 0; i < array.length; i++) {
+	cosole.log(array[i])
+}
